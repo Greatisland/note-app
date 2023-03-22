ea118411b052aea7f37aaebf120ad5268fc83a78
Hyeon JIN <dontsendloveletters@gmail.com>
2023-03-22 02:57:02 +0000

    json parse 이슈 확인

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -16,28 +16,49 @@ const NoteListContainer = styled.ul`
 
 `
 
+interface Notes {
+
+}
+
 interface NoteType {
   title : string,
   contents:string,
   date:string
 }
 
- const Main = () => {
-  const [noteData, setNoteData] = useState<NoteType []>()
 
-  useEffect(() => {
-    renderNotes()
-  },[])
+
+ const Main = () => {
+  let test = []
+  let noteDatas = []
 
   const renderNotes = () => {
-    let local = localStorage.getItem("솜사탕")
 
-    if (local !== null) {
-      const value = JSON.parse(local)
-      setNoteData([value])
+    for(let i = 0; i < localStorage.length; i++){
+      let key = localStorage.key(i)
+      let value = localStorage.getItem(key)
+      if(value !== null && value !== undefined){
+        // let jsonValue = JSON.parse(value)
+        test.push([key,value])
+      }
     }
+    noteDatas = test.map((value, i) => {
+      try {
+        let jsonValue = JSON.parse(value[1]);
+        return jsonValue
+      } catch (e) {
+        console.error(`Failed to parse JSON string: ${value[1]}`);
+      }
+    })
+    // let local = localStorage.getItem("솜사탕")
+    // if (local !== null) {
+    //   const value = JSON.parse(local)
+    //   setNoteData([value])
+    // }
   }
-
+  
+  renderNotes()
+  console.log(noteDatas)
 
   return (
     <>
@@ -45,11 +66,10 @@ interface NoteType {
         <SearchBar type="search" placeholder="Please typing keyword this place."></SearchBar>
       </form>
       {
-        noteData?.map((data, i) => (
+        noteDatas?.map((data, i) => (
           <ul key={i}>
             <li>{`${data.title} / ${data.contents}`}</li>
           </ul>
-          
         ))
       }
     </>
